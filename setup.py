bcd74cb7e939467e67b3fd8da7aa50e4ca0e8669
Panos Kountanis <panosktn@gmail.com>
2015-10-13 22:52:25 +0100

    distribute v0.1.0

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
-from mdx_video import __version__, __author__
+from mdx_audio import __version__, __author__
 
 setup(name='mdx_audio',
       version=__version__,
-      description='Markdown 2.0 extension for embedding audio files',
+      description='Markdown 2.0 extension for audio',
       author=__author__,
       author_email='panosktn@gmail.com',
-      url='https://github.com/pgk/python-markdown-soundmanager2',
+      url='https://github.com/pgk/python-markdown-audio',
       py_modules = ['mdx_audio'],
 )
