5bb2cbe4cbf3b99ced1a1ec354f50e4cd4c92908
Panos Kountanis <panosktn@gmail.com>
2015-10-17 23:13:28 +0100

    add `audiojs` tag

--- mdx_audio.py
+++ mdx_audio.py
@@ -11,6 +11,18 @@ Test One Audio tag
 >>> markdown.markdown(s, ['audio'])
 '<p><audio controls="controls"><source src="/url/1.mp3" type="audio/mp3"></source></audio></p>'
 
+Test One Audio tag for audiojs
+
+>>> s = "::audiojs[my title](/url/1.mp3)"
+>>> markdown.markdown(s, ['audio'])
+'<p><audio controls="controls"><source src="/url/1.mp3" type="audio/mp3"></source></audio></p>'
+
+Test does not handle unknown tags
+
+>>> s = "::audiofoo[my title](/url/1.mp3)"
+>>> '<audio>' in markdown.markdown(s, ['audio'])
+False
+
 Test Two Audio tags
 
 >>> s = "::audio[my title](/url/1.mp3 /url/2.ogg)"
@@ -30,23 +42,30 @@ from markdown.inlinepatterns import Pattern
 from markdown.util import etree
 
 
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 
 __author__ = 'Panos Kountanis'
 
 
 class AudioExtension(markdown.Extension):
+    pattern = r'(?:(?:^::+)){0}\[(?P<title>[\w\s\d+]*)\]\((?P<urls>[\w\d\W+]*)\)'
     configs = {}
-    def __init__(self, configs):
+    def __init__(self, configs=None):
         for k, v in configs or {}:
             self.setConfig(key, value)
 
     def extendMarkdown(self, md, md_globals):
-        pattern = r'(?:(?:^::+))(?P<format>[\w+-]*)\[(?P<title>[\w\s\d+]*)\]\((?P<urls>[\w\d\W+]*)\)'
-        inline_audio_pattern = AudioPattern(pattern)
+        self._add_inline('audio', self.pattern.format('audio'),
+                         AudioPattern, md, md_globals)
+        self._add_inline('audiojs', self.pattern.format('audiojs'),
+                         AudioPattern, md, md_globals)
+
+    def _add_inline(self, name, pattern, klass, md, md_globals):
+        inline_audio_pattern = klass(pattern)
+        inline_audio_pattern.md = md
         inline_audio_pattern.ext = self
-        md.inlinePatterns.add('audio', inline_audio_pattern, '<reference')
+        md.inlinePatterns.add(name, inline_audio_pattern, '<reference')
 
 
 class AudioPattern(markdown.inlinepatterns.Pattern):
@@ -54,7 +73,6 @@ class AudioPattern(markdown.inlinepatterns.Pattern):
         audio = etree.Element('audio')
         audio.set('controls', 'controls')
 
-        audio_format = m.group('format')
         audio_title = m.group('title')
         audio_urls = m.group('urls')
 
